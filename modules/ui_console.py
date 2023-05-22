e2b2fec237cec944dbec140a41dbb434cfbf4c90
Ahmed K. Hafsi <25119333+ahmed-hafsi@users.noreply.github.com>
2023-05-22 22:40:42 +0800

    Added ability to render trees

--- modules/ui_console.py
+++ modules/ui_console.py
@@ -1,5 +1,7 @@
 from rich.table import Table
 from rich.console import Console
+from rich.tree import Tree
+from rich import print
 
 def print_dataframe(df):
     console = Console()
@@ -16,3 +18,19 @@ def print_dataframe(df):
         table.add_row(*row.astype(str))
 
     console.print(table)
+
+# Create a tree in the console
+def create_tree(input,tree_name):
+    # Create a new Tree
+    tree = Tree(tree_name, style="bold blue")
+
+    
+    for feature, categories in input.items():
+        
+        feature_branch = tree.add(feature, style="bold green")
+        
+        for category in categories:
+            feature_branch.add(category, style="yellow")
+
+    
+    print(tree)
