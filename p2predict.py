f6b82de7f8374bec2bb18a2790d712d6f04dc3dc
Ahmed K. Hafsi <25119333+ahmed-hafsi@users.noreply.github.com>
2023-05-22 22:40:28 +0800

    expanded prediction UI in p2predict.py command.

--- p2predict.py
+++ p2predict.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 #UI
 import art
+import questionary
 from rich.console import Console
 from rich.table import Table
 from rich.prompt import Prompt
@@ -11,41 +12,75 @@ from rich.panel import Panel
 from rich.console import Group
 from rich.pretty import Pretty
 import click
+from modules.ui_console import create_tree
 
 # Model serialization
 import joblib
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
 
 def load_model(model_file):
-    model = joblib.load(model_file)
-    return model
+    model_metadata = joblib.load(model_file)
+    return model_metadata
 
 def predict(model,features):
     y = model.predict(features)
     return y
 
 @click.command()
-@click.option('--model', type=click.Path(exists=True), prompt='Enter model file path')
-@click.option('--features', prompt='Enter features to be predicted (format: key:value,key:value,...)')
-def main(model,features):
+@click.option('--model', type=click.Path(exists=True))
+@click.option('--features_inline')
+@click.option('--features_csv')
+def main(model,features_inline,features_csv):
     console = Console()
 
-    loaded_model = load_model(model)
+    if not model:
+        model = questionary.path('Enter model file path (.model file)').ask()
 
-    # Parse the features string into a dictionary
-    features_dict = dict(item.split(":") for item in features.split(","))
+    loaded_model_metadata = load_model(model)
+    trained_pipeline = loaded_model_metadata['model']
 
-    # Convert the dictionary into a DataFrame
-    features_df = pd.DataFrame([features_dict])
+    console.print(f"Model > { Pretty(loaded_model_metadata['model_name']) } loaded.", style="bold blue")
+    console.print(f"Model features: {loaded_model_metadata['features']}", style="bold blue")
+    console.print(f"Target feature: {loaded_model_metadata['target_feature']}", style="bold blue")
 
-    console.print(f'Model > {model} loaded.')
+    # Get the encoders for the categorical features
+    preprocessor = trained_pipeline.named_steps['preprocessor']
+    categorical_transformer = preprocessor.named_transformers_['cat']
+    onehot_encoder = categorical_transformer.named_steps['onehot']
+
+    # Get the trained features
+    trained_categories = onehot_encoder.categories_
+    transformers = preprocessor.transformers_
     
-    console.print(Panel(Pretty(loaded_model),title='Loaded Model'))
+    for name, transformer, feature_columns in transformers:
+        if isinstance(transformer, Pipeline) and isinstance(transformer.named_steps['onehot'], OneHotEncoder):
+            feature_columns = feature_columns.tolist()
 
-    y = predict(loaded_model,features_df)
+    all_categories = {}
+    
+    for i, feature in enumerate(feature_columns):
+        all_categories[feature] = trained_categories[i].tolist()
+
+    create_tree(all_categories,"Loaded Features: ")
+
+    features_dict = {}
+    if features_inline:
+        features_dict = dict(item.split(":") for item in features_inline.split(","))
+    else:     
+        for feature in loaded_model_metadata['features']:
+            features_inline = questionary.text(
+                    f'{feature}: ',
+                ).ask()
+            features_dict[feature] = features_inline
+        
+    features_df = pd.DataFrame([features_dict])   
+
+    y = predict(trained_pipeline,features_df)
 
     features_df['prediction'] = y
 
-    console.print(Panel(Pretty(features_df),title="Data Output"))
+    console.print(Panel(Pretty(features_df),title="Model Prediction"))
     
     return y
 
